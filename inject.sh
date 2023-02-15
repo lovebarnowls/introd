bdb203fcf834c56a9c9a43d87c8745b2f14bf94b
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-02-15 13:33:43 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -3,15 +3,15 @@
 sudo useradd ron
 sudo useradd idontbelonghere
 sudo useradd chris
-sudo useradd ivan
+sudo useradd yang
 sudo useradd james
 sudo useradd sam
 sudo passwd -d ron
 sudo usermod -aG sudo ron
 sudo usermod -aG sudo sam
-sudo mkdir -p /home/ivan/desktop/iloveqld
+sudo mkdir -p /home/yang/desktop/iloveqld
 sudo mkdir -p /home/chris/desktop/ilovevic/vicisawesome
-sudo echo 'you should not have opened this' > /home/ivan/desktop/iloveqld/clickonthistomakeyourdreamscometrue.dll
+sudo echo 'you should not have opened this' > /home/yang/desktop/iloveqld/clickonthistomakeyourdreamscometrue.dll
 sudo echo 'you should not open this either' > /home/chris/desktop/ilovevic/vicisawesome/thisisabackdoor.exe
 sudo echo 'thisisforadvancedquestion1' > /home/chris/desktop/ilovevic/lullaby.txt
 sudo chown :1006 /home/chris/desktop/ilovevic/lullaby.txt
