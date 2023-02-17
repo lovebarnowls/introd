d59dec2c76ade9d1565e09d781f3035c0b7d518f
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-02-17 11:41:09 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -12,9 +12,9 @@ sudo mkdir -p /home/yang/desktop/iloveqld
 sudo mkdir -p /home/chris/desktop/ilovevic/vicisawesome
 sudo echo 'you should not have opened this' > /home/yang/desktop/iloveqld/clickonthistomakeyourdreamscometrue.dll
 sudo echo 'you should not open this either' > /home/chris/desktop/ilovevic/vicisawesome/thisisabackdoor.exe
-sudo echo 'thisisforadvancedquestion1' > /home/chris/desktop/ilovevic/lullaby.txt
+sudo echo 'thisisforadvancedquestionsection' > /home/chris/desktop/ilovevic/cyberiscool.txt
 sudo chown :1006 /home/chris/desktop/ilovevic/lullaby.txt
 sudo mkdir -p /home/james/desktop/
-sudo echo 'welcometocybertaipantraining' |base64 > /home/james/desktop/welcome.txt
+sudo echo 'thisisinbase64' |base64 > /home/james/desktop/welcome.txt
 
 
